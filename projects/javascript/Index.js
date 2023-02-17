c5271cf23083dfcddf993274053344a5776d3da3
Shubham <shubhamvaghani555@gmail.com>
2023-02-17 10:39:17 +0530

    second change

---
+++ projects/javascript/Index.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+console.error("This is an error")
+console.log("This is a log")
+
+var a = 15 // var Is Having Global Scope
+let b = 20 // let Is Having block scope
+const ifsc = "SBI0404565" //For The Constant Value & const Is having block scope
+
+// For Print The Value 
+
+console.log("The Value Of the a = ",a)// it will print a
+console.log("The Value Of The b =",b)// it will print b
+console.log("The Value Of The IFSC =",ifsc)// it will print ifsc
+
+    
+
+
+
